06abfa393cdadce166b14c03beba80f92b1f5b53
Windows8r <134237377+Windows8r@users.noreply.github.com>
2023-07-08 10:06:04 +0700

    Create idk.bat

---
+++ idk.bat
@@ -0,0 +1,13 @@
+sudo apt update && sudo apt upgrade
+sudo apt install wget
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./chrome-remote-desktop_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+ 
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+ 
+ 
+sudo systemctl disable lightdm.service
